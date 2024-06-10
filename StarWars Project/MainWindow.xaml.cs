8b9ecedbeda9f674477b3053c7eff42860d125a1
BaseMar <msmsebastiani@gmail.com>
2024-06-10 09:23:49 +0200

    Dodaj pliki projektów.

---
+++ StarWars Project/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using StarWars_Project.ViewModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StarWars_Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            NavigationMenu_O = new NavigationMenu();
+        }
+
+        public NavigationMenu NavigationMenu_O { get; set; }
+
+        private void btnCharacterSheet_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = NavigationMenu_O.CharacterSheetVM_O;
+        }
+
+        private void btnMap_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = NavigationMenu_O.MapVM_O;
+        }
+
+        private void btnSettings_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = NavigationMenu_O.SettingsVM_O;
+        }
+
+        private void btnMusic_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = NavigationMenu_O.MusicVM_O;
+        }
+
+        private void btnMonsters_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = NavigationMenu_O.MonstersVM_O;
+        }
+    }
+}
---
+++ StarWars Project/ViewModel/NavigationMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarWars_Project.ViewModel
+{
+    public class NavigationMenu
+    {
+        public CharacterSheetVM CharacterSheetVM_O { get; set; }
+        public MapVM MapVM_O { get; set; }
+        public SettingsVM SettingsVM_O { get; set; }
+        public MusicVM MusicVM_O { get; set; }
+        public MonstersVM MonstersVM_O { get; set; }
+        public NavigationMenu()
+        {
+            CharacterSheetVM_O = new CharacterSheetVM();
+            MapVM_O = new MapVM();
+            SettingsVM_O = new SettingsVM();
+            MusicVM_O = new MusicVM();
+            MonstersVM_O = new MonstersVM();
+        }
+    }
+}
